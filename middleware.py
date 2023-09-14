01e5171a678369d87d6ae91f0f570c2879b2e39a
FarisHijazi <theefaris@gmail.com>
2023-09-14 16:20:11 -0700

    added timeout in attempt to improve middleware

--- middleware.py
+++ middleware.py
@@ -4,12 +4,18 @@ All this file does is take the requests and forwrads them but changes a single p
 
 More functionality should be added later such as keep track of context of multiple files and maintaining a user session,
   but this would need lots of experimenting.
+
+    pip install -U httpx -U fastapi -U uvicorn -U websockets
+    python middleware.py --port 8000
+
 """
 
 import requests
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 import httpx
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL)
 
 
 app = FastAPI()
@@ -21,58 +27,46 @@ async def code_completion(body: dict):
     if "max_tokens" in body:
         del body["max_tokens"]
 
+    # FIXME: this is just a hardcoded number, but this should actually use the tokenizer to truncate
+    body["prompt"] = body["prompt"][-4000:]
     print("making request. body:", {k: v for k, v in body.items() if k != "prompt"})
 
     def code_completion_stream(body: dict):
         # define the generator for streaming
         async def stream_content():
-            async with httpx.AsyncClient() as client:
-                for i in range(body["n"]):
-                    # FIXME: this is just a hardcoded number, but this should actually use the tokenizer to truncate
-                    body["prompt"] = body["prompt"][-2048:]
-                    async with client.stream(
-                        "POST",
-                        "http://localhost:5001/v1/codegen/completions",
-                        json=body,
-                        headers={
-                            "Accept": "application/json",
-                            "Content-type": "application/json",
-                        },
-                    ) as response:
-                        # Check if the response status is not successful
-                        if response.status_code != 200:
-                            raise HTTPException(
-                                status_code=response.status_code,
-                                detail="Failed to fetch from the target endpoint",
-                            )
+            try:
+                async with httpx.AsyncClient(timeout=30.0) as client:
+                    for i in range(body["n"]):
+                        async with client.stream(
+                            "POST",
+                            "http://localhost:5001/v1/codegen/completions",
+                            json=body,
+                            headers={
+                                "Accept": "application/json",
+                                "Content-type": "application/json",
+                            },
+                        ) as response:
+                            # Check if the response status is not successful
+                            if response.status_code != 200:
+                                raise HTTPException(
+                                    status_code=response.status_code,
+                                    detail="Failed to fetch from the target endpoint",
+                                )
 
-                        # Stream the response content
-                        async for chunk in response.aiter_bytes():
-                            # print('getting chunk')
-                            print(f"{chunk=}")
-                            yield chunk
+                            # Stream the response content
+                            async for chunk in response.aiter_bytes():
+                                # print('getting chunk')
+                                print(f"{chunk=}")
+                                yield chunk
+            except httpx.ReadTimeout:
+                print("A timeout occurred while reading data from the server.")
 
         return StreamingResponse(stream_content(), media_type="application/json")
-
-    def code_completion_nostream(body: dict):
-        response = requests.post(
-            "http://localhost:5001/v1/engines/codegen/completions",
-            {
-                "method": "POST",
-                "headers": {
-                    "Accept": "application/json",
-                    "Content-type": "application/json",
-                },
-                "body": body,
-            },
-        )
-        print("response", response, response.content)
-        return response.content
 
     if "stream" in body and body["stream"]:
         return code_completion_stream(body)
     else:
-        return "data: " + code_completion_nostream(body)
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
