bb0f2fdae4eb248912f1258f3e6d71cf8c13fbfd
o0oradaro0o <l.vanatta@gmail.com>
2023-10-05 08:22:47 -0500

    try changing the setup to include src

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import setuptools
 with open('README.md','r', encoding="utf8") as fh:
     long_description = fh.read().replace('](', '](https://raw.githubusercontent.com/FarisHijazi/PrivateGitHubCopilot/master/')
 with open('requirements.txt','r', encoding="utf8") as fh:
-    rqeuirements = fh.readlines()
+    requirements = fh.readlines()
 
 version = subprocess.Popen('git describe --abbrev=0 --tags', shell=True, stdout=subprocess.PIPE).stdout.read().decode().strip().lstrip('v')
 
@@ -20,9 +20,9 @@ setuptools.setup(
     author='Faris Hijazi',
     author_email='theefaris@gmail.com',
     long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
     python_requires='>=3.6',
-    install_requires=rqeuirements,
+    install_requires=requirements,
     keywords='',
     entry_points={
         'console_scripts': [
